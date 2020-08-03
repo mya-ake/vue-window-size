7b46dcc23a1145c5b48fc4de4c315ccd2eb17f38
mya-ake <make.app.ts@gmail.com>
2020-08-03 23:49:42 +0900

    refactor: use Vue.observable

--- src/mixin.ts
+++ src/mixin.ts
@@ -1,8 +1,20 @@
 import Vue from 'vue';
 import WindowSize from './window-size';
 
+type Vm = {
+  windowSize: {
+    width: number;
+    height: number;
+  };
+};
+
 export const windowSize = new WindowSize().init();
-const vm = new Vue({ data: { windowSize } });
+
+const vm = ((): Vm => {
+  return typeof Vue.observable === 'function'
+    ? Vue.observable({ windowSize })
+    : new Vue({ data: { windowSize } });
+})();
 
 export const mixin = {
   computed: {
