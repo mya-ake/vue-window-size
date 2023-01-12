275e58bf4f005ce5474636b49f63cd8bbbbedeaf
mya-ake <myaaake@gmail.com>
2023-01-12 20:56:38 +0900

    chore: create example for composition api

---
+++ examples/composition-api/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useWindowSize } from 'vue-window-size'
+
+const { width: windowWidth, height: windowHeight } = useWindowSize();
+</script>
+
+<template>
+  <div>
+    <p>width: {{ windowWidth }}</p>
+    <p>height: {{ windowHeight }}</p>
+  </div>
+</template>
--- packages/vue-window-size/vite.config.ts
+++ packages/vue-window-size/vite.config.ts
@@ -3,26 +3,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { resolve } from 'path';
-
-const root = process.cwd();
 
 export default defineConfig({
   plugins: [vue(), tsconfigPaths()],
-  build: {
-    lib: {
-      entry: resolve(root, 'src/index.ts'),
-      name: 'vue-window-size',
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  },
   test: {
     environment: 'happy-dom',
   },
