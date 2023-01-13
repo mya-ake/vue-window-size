a6157b4c6208e54f61f62860a9a9afa87e75d1be
mya-ake <myaaake@gmail.com>
2023-01-13 18:34:06 +0900

    chore: create example for mixin

---
+++ examples/option-api/src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { vueWindowSizeMixin } from 'vue-window-size/option-api'
+
+export default defineComponent({
+  mixins: [vueWindowSizeMixin()]
+})
+</script>
+
+<template>
+  <div>
+    <p>width: {{ $windowWidth }}</p>
+    <p>height: {{ $windowHeight }}</p>
+  </div>
+</template>
