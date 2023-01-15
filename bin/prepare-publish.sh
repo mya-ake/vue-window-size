64dd554abd81c140f15cf5b0177c32c212ef86f0
mya-ake <myaaake@gmail.com>
2023-01-15 23:11:29 +0900

    fix: packages

--- bin/prepare-publish.sh
+++ bin/prepare-publish.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-LIBS_DIR=libs
+packages="composition-api plugin mixin"
 
-rm -rf $LIBS_DIR
-mkdir $LIBS_DIR
-
-LIBS="composition-api plugin mixin"
-
-for lib in $LIBS
+for package in $packages
 do
-  mkdir $LIBS_DIR/$lib
-  cp packages/$lib/package.json $LIBS_DIR/$lib/package.json
-  cp -r packages/$lib/dist $LIBS_DIR/$lib/dist
+  rm -rf ./$package
+  mkdir $package
+  cp ./packages/$package/package.json ./$package/package.json
+  cp -r ./packages/$package/dist ./$package/dist
 done
