4356d85ceadda3efd121e8a24254751b71588480
mya-ake <myaaake@gmail.com>
2023-01-16 23:12:50 +0900

    refactor: plugin type

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -2,12 +2,13 @@ import { createMixin } from '~shared/mixin';
 import { getSubject } from '~shared/subject';
 import { createPublicAPI } from '~shared/public-api';
 import type { Config } from '~shared/config';
+import type { App } from 'vue';
 
 // Public API
 export const vueWindowSizeAPI = createPublicAPI(getSubject);
 
 // plugin
-function install(app: any, { delay = 33 }: Config = {}) {
+function install(app: App, { delay = 33 }: Config = {}) {
   const mixin = createMixin(getSubject);
   getSubject().setDelay(delay);
   app.mixin({
@@ -17,8 +18,7 @@ function install(app: any, { delay = 33 }: Config = {}) {
 
 export const VueWindowSizePlugin = { install };
 
-/** types */
-declare module '@vue/runtime-core' {
+declare module 'vue' {
   export interface ComponentCustomProperties {
     $windowWidth: number;
     $windowHeight: number;
